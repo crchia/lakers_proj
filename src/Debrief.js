a627ae4dfd0e2c38a436da236c382c8c58624ace
Cooper Chia <crchia@uw.edu>
2022-01-19 12:44:36 -0800

    fixing debrief spacing

--- src/Debrief.js
+++ src/Debrief.js
@@ -3,15 +3,15 @@ const Debrief = () => {
     <div className="debrief">
       <h2>Debrief</h2>
       <ul>
-        <li style={{marginBottom: '10'}}>The above visualization details the Lakers' scoring rate <br/> 
+        <li style={{marginBottom: '10px'}}>The above visualization details the Lakers' scoring rate <br/> 
         for possessions directly terminated by a Jon Smith drive/post-up</li>
-        <li style={{marginBottom: '10'}}>Note 1: Smith's creation/efficiency in the playoffs was unbelievable, <br/>
+        <li style={{marginBottom: '10px'}}>Note 1: Smith's creation/efficiency in the playoffs was unbelievable, <br/>
         an indicator that he isn't vulnerable to playoff schemes/defenses.</li>
-        <li style={{marginBottom: '10'}}>Note 2: Smith not only prefers driving left as opposed to right, <br/>
+        <li style={{marginBottom: '10px'}}>Note 2: Smith not only prefers driving left as opposed to right, <br/>
         but the points generated off of his left drives is also significantly higher. </li>
-        <li style={{marginBottom: '10'}}>Note 2.5: The same left vs right preference can be applied to Smith's <br/>
+        <li style={{marginBottom: '10px'}}>Note 2.5: The same left vs right preference can be applied to Smith's <br/>
         post-ups. Efficiency rises dramatically on left-side-of court post-ups</li>
-        <li style={{marginBottom: '10'}}>Conclusion: Smith is more than capable of carrying a high offensive load <br/>
+        <li style={{marginBottom: '10px'}}>Conclusion: Smith is more than capable of carrying a high offensive load <br/>
         in the playoffs, a particularly valuable trait. <br/>
         Smith clearly has specific hot spots, and letting him create from these spots <br/>
         yields significantly increased team production.</li>
